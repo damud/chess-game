101d2398eb8e9614f3ef2604fa301ec9a41c400c
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-05-22 11:50:04 +0200

    Created chess board

---
+++ app.js
@@ -0,0 +1,81 @@
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplaty = document.querySelector("#info-display");
+
+const startPieces = [
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+];
+
+const createBoard = () => {
+  startPieces.forEach((startPiece) => {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.classList.add("beige");
+    gameBoard.append(square);
+  });
+};
+
+createBoard();
